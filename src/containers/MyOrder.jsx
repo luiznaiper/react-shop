0457698b0b3d9fb9e79bdf6d3d1fecb8069076ec
luiznaiper <luiz_naiper@hotmail.com>
2022-06-07 07:09:28 -0500

    :construction: Added sum total function to myorder toggle

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -6,6 +6,11 @@ import arrowIcon from "@icons/arrow.svg";
 
 const MyOrder = () => {
   const { state } = useContext(AppContext);
+  const sumTotal = () => {
+    const reducer = (acc, el) => acc + el.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -13,14 +18,14 @@ const MyOrder = () => {
         <p className="title">My order</p>
       </div>
       <div className="my-order-content">
-        {state.cart.map((item) => (
-          <OrderItem product={item} key={`orderItem-${item.id}`} />
+        {state.cart.map((product) => (
+          <OrderItem product={product} key={`orderItem-${product.id}`} />
         ))}
         <div className="order">
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
