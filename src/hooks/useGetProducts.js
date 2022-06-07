9e417c6b2f851e7f8501f6f008d84315993155f6
luiznaiper <luiz_naiper@hotmail.com>
2022-06-07 07:13:18 -0500

    :bug: Fixed the alert of async function

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -4,11 +4,27 @@ import axios from 'axios';
 const useGetProducts = (API) => {
 const [products, setProducts] = useState([]);
 
-useEffect(async () => {
-  const response = await axios(API);
+useEffect( () => {
+  async function fetchData(){
+    const response = await axios(API);
   setProducts(response.data);
+  }
+ fetchData() 
 }, []);
 return products;
 }
 
 export {useGetProducts}
+
+/*react_devtools_backend.js:4026 Warning: useEffect must not return anything besides a function, which is used for clean-up.
+
+It looks like you wrote useEffect(async () => ...) or returned a Promise. Instead, write the async function inside your effect and call it immediately:
+
+useEffect(() => {
+  async function fetchData() {
+    // You can await here
+    const response = await MyAPI.getData(someId);
+    // ...
+  }
+  fetchData();
+}, [someId]); // Or [] if effect doesn't need props or state */
